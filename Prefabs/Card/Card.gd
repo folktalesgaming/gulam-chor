f970a6f8db77b8fbabc7156e9d6d7b9d8d5fc89a
alsongarbuja <garbuja12alson@gmail.com>
2023-10-23 13:44:47 +0545

    prefabs: update card prefab to have back skin

--- Prefabs/Card/Card.gd
+++ Prefabs/Card/Card.gd
@@ -1,8 +1,14 @@
 extends MarginContainer
 
-var cardName = "card_a_spade";
+var cardName = "card_a_spade"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _ready():
 	$Card.texture = load("res://Assets/UI/Cards/"+cardName+".png")
 	$Card.scale *= size/$Card.texture.get_size()
+	
+	$CardBack.texture = load("res://Assets/UI/Cards/new_card_back.png")
+	$CardBack.scale *= size/$CardBack.texture.get_size()
+
+static func SetIsMyCard():
+	$CardBack.visible = false
