fca78d10c3aae498ec59935a3e63fd2db0f56dfb
alsongarbuja <garbuja12alson@gmail.com>
2023-10-23 13:45:38 +0545

    WIP: add logic to shuffle deck and divide into 4 players
    - Still need to make the card in main player hand more aligned
    - Still need to make the card remove pairs after the division
    - Still need multiplayer part and all other main logics

--- Scenes/Game_play.gd
+++ Scenes/Game_play.gd
@@ -1,20 +1,48 @@
 extends Node2D
 
 const CardBase = preload("res://Prefabs/Card/Card.tscn")
+const Utility = preload("res://Utils/utility.gd")
 const DeckOfCard = preload("res://Utils/deck_of_cards_classic.gd")
 
-@onready var CenterCardOval = ProjectSettings.get_setting("display/window/size/viewport_width") * Vector2(0.6, 1)
-@onready var Horizontal_radius = get_viewport().size.x * 0.45
-@onready var Vertical_radius = get_viewport().size.y * 0.4
+@onready var deckOfCards = Utility.shuffleDeck(DeckOfCard.getDeckClassic())
+@onready var ViewportSize = ProjectSettings.get_setting("display/window/size/viewport_width")
+#Vector2(get_viewport().size.x, get_viewport().size.y)
+@onready var CenterCardOval = ViewportSize * Vector2(0.5, 1.55)
+@onready var Horizontal_radius = ViewportSize * 0.45
+@onready var Vertical_radius = ViewportSize * 0.3
 var angle = deg_to_rad(90) - 0.5
 var OvalAngleVector = Vector2()
+var playerIndex = 0
+var posOffsetYRight = 0
+var posOffsetYLeft = 0
+var posOffsetX = 0
 
 func _ready():
-	for card in DeckOfCard.deck:
+	for card in deckOfCards:
 		var new_card = CardBase.instantiate()
 		new_card.cardName = card
-		OvalAngleVector = Vector2(Horizontal_radius * cos(angle), - Vertical_radius * sin(angle))
-		new_card.position = CenterCardOval + OvalAngleVector - new_card.size/2
-		new_card.rotation = (90 - rad_to_deg(angle))/4
-		$PlayerCards.add_child(new_card)
-		angle += 0.25
+		
+		if playerIndex == 0:
+			# TODO: fix the angles for the card also make it more dynamic for later when removing pairs and adding cards to hand 
+			OvalAngleVector = Vector2(Horizontal_radius * cos(angle), - Vertical_radius * sin(angle))
+			new_card.position = CenterCardOval + OvalAngleVector
+			new_card.rotation = deg_to_rad(angle)/4
+			new_card.SetIsMyCard()
+			$PlayerCards.add_child(new_card)
+			angle += 0.25
+		if playerIndex == 1:
+			new_card.position = ViewportSize * Vector2(0.95, 1 - posOffsetYRight)
+			new_card.rotation = deg_to_rad(90)
+			$SecondPlayer.add_child(new_card)
+			posOffsetYRight += 0.07
+		if playerIndex == 2:
+			new_card.position = ViewportSize * Vector2(0.75 - posOffsetX, -0.1)
+			$ThirdPlayer.add_child(new_card)
+			posOffsetX += 0.05
+		if playerIndex == 3:
+			new_card.position = ViewportSize * Vector2(-0.08, 1 - posOffsetYLeft)
+			new_card.rotation = deg_to_rad(90)
+			$FourthPlayer.add_child(new_card)
+			posOffsetYLeft += 0.07
+		
+		playerIndex = (1+playerIndex)%4
