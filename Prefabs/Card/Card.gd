696df91985af2bdeb497552239e88da37951d109
alsongarbuja <garbuja12alson@gmail.com>
2023-10-25 13:22:19 +0545

    WIP: adding animation for deck division among players

--- Prefabs/Card/Card.gd
+++ Prefabs/Card/Card.gd
@@ -1,8 +1,25 @@
 extends MarginContainer
 
 var cardName = "card_a_spade"
+var startPosition = 0
+var targetPosition = 0
+var startRotation = 0
+var targetRotation = 0
+var DRAWTIME = 1
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+enum {
+	INDECK,
+	MOVINGFROMDECKTOHAND,
+	INHAND,
+	INPAIR,
+	MOVINGFROMHANDTOPAIR,
+	INPICK,
+	MOVINGFROMPICKTOHAND,
+	REORGANIZE,
+	SHUFFLE,
+}
+var state = INDECK
+
 func _ready():
 	$Card.texture = load("res://Assets/UI/Cards/"+cardName+".png")
 	$Card.scale *= size/$Card.texture.get_size()
@@ -12,3 +29,32 @@ func _ready():
 
 static func SetIsMyCard():
 	$CardBack.visible = false
+
+func _physics_process(delta):
+	match state:
+		INDECK:
+			pass
+		MOVINGFROMDECKTOHAND:
+			animateFromStartToTarget()
+		INHAND:
+			pass
+		INPAIR:
+			pass
+		MOVINGFROMHANDTOPAIR:
+			animateFromStartToTarget()
+		INPICK:
+			pass
+		MOVINGFROMPICKTOHAND:
+			pass
+		REORGANIZE:
+			animateFromStartToTarget()
+		SHUFFLE:
+			pass
+
+func animateFromStartToTarget():
+	var tween = get_tree().create_tween()
+	tween.set_parallel(true)
+	tween.tween_property($".", "position", targetPosition, DRAWTIME).from(startPosition).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property($".", "rotation", targetRotation, DRAWTIME-0.5).from(startRotation).set_trans(Tween.TRANS_CUBIC)
+	
+	state = INHAND
--- Scenes/Game_play.gd
+++ Scenes/Game_play.gd
@@ -22,47 +22,68 @@ const RightPlayerCardPositionOffset = 0.07
 const TopPlayerCardPositionOffset = 0.05
 const LeftPlayerCardPositionOffset = 0.07
 
+var rng = RandomNumberGenerator.new()
+
 # TODO: make it proper player prefabs and player models to represent them
 var player = [];
 var playerRight = [];
 var playerLeft = [];
 var playerTop = [];
+var removedPairCards = [];
+
+enum {
+	INDECK,
+	MOVINGFROMDECKTOHAND,
+	INHAND,
+	INPAIR,
+	MOVINGFROMHANDTOPAIR,
+	INPICK,
+	MOVINGFROMPICKTOHAND,
+	REORGANIZE,
+	SHUFFLE,
+}
 
 func _ready():
 	# TODO: add animations to divide the cards into players
 	for card in deckOfCards:
 		var new_card = CardBase.instantiate()
 		new_card.cardName = card
+		new_card.startPosition = $PackOfDeck.position
+		new_card.startRotation = 0
 		
 		if playerIndex == 0:
 			# TODO: Make it more dynamic for later when removing pairs and adding cards to hand 
 			OvalAngleVector = Vector2(-Horizontal_radius * cos(angle), -Vertical_radius * sin(angle))
-			new_card.position = CenterCardOval - OvalAngleVector
-			new_card.rotation = deg_to_rad(angle)/2
+			new_card.targetPosition = CenterCardOval - OvalAngleVector
+			new_card.targetRotation = deg_to_rad(angle)/2
 			new_card.SetIsMyCard()
 			$PlayerCards.add_child(new_card)
 			player.append(card)
 			angle += PlayerHandCardAngleOffset
 		if playerIndex == 1:
-			new_card.position = ViewportSize * Vector2(0.95, 1 - posOffsetYRight)
-			new_card.rotation = deg_to_rad(90)
+			new_card.targetPosition = ViewportSize * Vector2(0.95, 1 - posOffsetYRight)
+			new_card.targetRotation = deg_to_rad(90)
 			$SecondPlayer.add_child(new_card)
 			playerRight.append(card)
 			posOffsetYRight += RightPlayerCardPositionOffset
 		if playerIndex == 2:
-			new_card.position = ViewportSize * Vector2(0.75 - posOffsetX, -0.1)
+			new_card.targetPosition = ViewportSize * Vector2(0.75 - posOffsetX, -0.1)
 			$ThirdPlayer.add_child(new_card)
 			playerTop.append(card)
 			posOffsetX += TopPlayerCardPositionOffset
 		if playerIndex == 3:
-			new_card.position = ViewportSize * Vector2(-0.08, 1 - posOffsetYLeft)
-			new_card.rotation = deg_to_rad(90)
+			new_card.targetPosition = ViewportSize * Vector2(-0.08, 1 - posOffsetYLeft)
+			new_card.targetRotation = deg_to_rad(90)
 			playerLeft.append(card)
 			$FourthPlayer.add_child(new_card)
 			posOffsetYLeft += LeftPlayerCardPositionOffset
+			
+		new_card.state = MOVINGFROMDECKTOHAND
 		
 		playerIndex = (1+playerIndex)%4
 		
+	$PackOfDeck.visible = false
+
 	# TODO: add animations for removing pairs of cards into the center pile
 	player = Utility.removePairs(player)
 	removeCardsFromScreen($PlayerCards, player)
@@ -74,10 +95,17 @@ func _ready():
 	removeCardsFromScreen($ThirdPlayer, playerTop)
 	
 	playerLeft = Utility.removePairs(playerLeft)
-	removeCardsFromScreen($FourthPlayer, playerLeft)	
+	removeCardsFromScreen($FourthPlayer, playerLeft)
 
 # TODO: adjust the position of remaining cards in hands for each players after removing pairs
 func removeCardsFromScreen(node, toCheckCardList):
 	for card in node.get_children():
 		if not toCheckCardList.has(card.cardName):
+			card.startPosition = card.position
+			card.targetPosition = Vector2($PairCards.position.x + rng.randf_range(-2.0, 2.0), $PairCards.position.y + rng.randf_range(-1.0, 1.0))
+			card.startRotation = card.rotation
+			card.targetRotation = deg_to_rad(rng.randf_range(-130.0, 130.0))
+			card.SetIsMyCard()
+			card.state = MOVINGFROMHANDTOPAIR
+			removedPairCards.append(card)
 			card.free()
