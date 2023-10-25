fc8c87f11dba672e1b44ee2db8afd45f87df2bf4
alsongarbuja <garbuja12alson@gmail.com>
2023-10-25 17:44:08 +0545

    prefab: update the prefab to have a paired state with red shadows

--- Prefabs/Card/Card.gd
+++ Prefabs/Card/Card.gd
@@ -3,7 +3,7 @@ extends MarginContainer
 var cardName = "card_a_spade"
 var startPosition = 0
 var targetPosition = 0
-var startRotation = 0
+var startRotation = deg_to_rad(0)
 var targetRotation = 0
 var DRAWTIME = 1
 
@@ -20,6 +20,8 @@ enum {
 }
 var state = INDECK
 
+var tween
+
 func _ready():
 	$Card.texture = load("res://Assets/UI/Cards/"+cardName+".png")
 	$Card.scale *= size/$Card.texture.get_size()
@@ -30,31 +32,43 @@ func _ready():
 static func SetIsMyCard():
 	$CardBack.visible = false
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	match state:
 		INDECK:
 			pass
 		MOVINGFROMDECKTOHAND:
-			animateFromStartToTarget()
+			animateFromStartToTarget(INHAND)
 		INHAND:
 			pass
 		INPAIR:
-			pass
+			animateFromStartToTarget(INHAND, false, true)
 		MOVINGFROMHANDTOPAIR:
-			animateFromStartToTarget()
+			animateFromStartToTarget(INDECK)
 		INPICK:
 			pass
 		MOVINGFROMPICKTOHAND:
 			pass
 		REORGANIZE:
-			animateFromStartToTarget()
+			animateFromStartToTarget(INHAND)
 		SHUFFLE:
 			pass
 
-func animateFromStartToTarget():
-	var tween = get_tree().create_tween()
+func animateFromStartToTarget(nextState, shouldRotate=true, selected=false):
+	if tween:
+		tween.kill()
+		
+	tween = create_tween()
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.set_trans(Tween.TRANS_CUBIC)
 	tween.set_parallel(true)
-	tween.tween_property($".", "position", targetPosition, DRAWTIME).from(startPosition).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property($".", "rotation", targetRotation, DRAWTIME-0.5).from(startRotation).set_trans(Tween.TRANS_CUBIC)
 	
-	state = INHAND
+	tween.tween_property($".", "position", targetPosition, DRAWTIME).from(startPosition)
+	if shouldRotate:
+		tween.tween_property($".", "rotation", targetRotation, DRAWTIME-0.5).from(startRotation)
+	else:
+		if selected:
+			tween.tween_property($SelectedContainer, "visible", true, DRAWTIME-0.7).from(false)
+		else:
+			tween.tween_property($SelectedContainer, "visible", false, DRAWTIME-0.7).from(true)
+	
+	state = nextState
