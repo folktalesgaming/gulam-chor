52a09842104054254e273c1f7d72870a9a25a6d1
alsongarbuja <garbuja12alson@gmail.com>
2023-11-13 19:00:35 +0545

    update-refactor: update the main bg with lighter bg and some code refactor

--- Scenes/ExitButton.gd
+++
@@ -1,6 +0,0 @@
-extends TextureButton
-
-# TODO: add click sound effects in all buttons
-func _on_pressed():
-	$"../../ButtonClick".play()
-	$"../PanelContainer".show()
--- Scenes/StartButton.gd
+++
@@ -1,6 +0,0 @@
-extends TextureButton
-
-func _on_pressed():
-	$"../../ButtonClick".play()
-	await get_tree().create_timer(0.2).timeout
-	get_tree().change_scene_to_file("res://Scenes/Game_play.tscn")
