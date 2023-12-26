c5e83c52057965723785c0d4d4f072a3a3ed216b
alsongarbuja <garbuja12alson@gmail.com>
2023-12-26 19:58:23 +0545

    remove: remove old and unwanted scripts

--- Prefabs/DragZone/Drag_Zone.gd
+++
@@ -1,8 +0,0 @@
-extends StaticBody2D
-
-@export var dragging_zone = "pile"
-
-func _on_area_2d_body_entered(body):
-	print("dragging_zone = %s"%body.dragging_zone)
-	if body.dragging_zone == dragging_zone:
-		print("can drop")
